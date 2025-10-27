1e9d2925443c37245fba471d7df4c6d1a883f2e0
Iris <148431646+iris915520@users.noreply.github.com>
2025-10-28 01:35:46 +0200

    restful api

--- SpringBoot/server/src/main/java/ssp/assignment3/server/controler/ProductController.java
+++ SpringBoot/server/src/main/java/ssp/assignment3/server/controler/ProductController.java
@@ -1,9 +1,52 @@
 package ssp.assignment3.server.controler;
 
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import ssp.assignment3.server.data.model.Product;
+import ssp.assignment3.server.data.model.ProductCollection;
+import ssp.assignment3.server.db.Database;
+
+@RestController
+@RequestMapping("/products")
 public class ProductController {
 
-	public ProductController() {
-		// TODO Auto-generated constructor stub
+	@GetMapping(value = "/{unit_price}", produces = MediaType.APPLICATION_XML_VALUE)
+	public ProductCollection getProduct(@PathVariable("unit_price") double unit_price) {
+		List<Product> result = Database.searchByPrice(unit_price);
+		ProductCollection productCollection = new ProductCollection();
+		productCollection.setProductList(result);
+		return productCollection;
 	}
-
+	
+	@GetMapping(value = "/cheapest", produces=MediaType.APPLICATION_XML_VALUE)
+	public Product getCheapest() {
+		return Database.getCheapest();
+	}
+	
+	@GetMapping(value = "/expensive&cheapest", produces=MediaType.APPLICATION_JSON_VALUE)
+	public Map<String, Product> getExpensiveAndCheapest() {
+		Product cheapest = Database.getCheapest();
+		Product expensive = Database.getExpensive();
+		Map<String, Product> map = new ConcurrentHashMap<>();
+		map.put("expensive", expensive);
+		map.put("cheapest", cheapest);
+		return map;
+	}
+	
+	@GetMapping(value = "/list", produces=MediaType.APPLICATION_XML_VALUE)
+	public ProductCollection getList() {
+		List<Product> list = Database.list();
+		ProductCollection productCollection = new ProductCollection();
+		productCollection.setProductList(list);
+		return productCollection;
+	}
+	
 }
